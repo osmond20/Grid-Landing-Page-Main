53679402fc5f5345140a5ac51ad1b97bf201d61f
Nkosinathi Mothudi <osmondmothudi@gmail.com>
2026-08-13 09:21:56 +0200

    added code to JS

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
 const menuButton = document.querySelector(".menu-button");
 const menu = document.querySelector(".menu");
 
+// Menu functionality
 menuButton.addEventListener("click", () =>{
-    menu.classList.toggle("show");
-    menu.setAttribute('aria-expanded', true);
+    // stores the boolean result of the toggle
+    const isOpen = menu.classList.toggle("show");
+    // evaluates the result
+    if(isOpen === true){
+        menuButton.setAttribute('src', "assets/images/icon-close.svg");
+        menu.setAttribute('aria-expanded', isOpen);
+    }
+    else{
+        menuButton.setAttribute('src', "assets/images/icon-menu.svg");
+    } 
 });
